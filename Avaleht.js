2072c9f0485f30ace301f45f3719f24a6e91e1ee
maksimjakovlev <44438021+maksimjakovlev@users.noreply.github.com>
2021-11-29 23:13:40 +0200

    Add files via upload

---
+++ Avaleht.js
@@ -0,0 +1,36 @@
+function Avavaheleht1(){
+    window.location=("./Vahelehed/ut.html")
+}
+function Avavaheleht9(){
+    window.location=("./Vahelehed/TartuTK.html")
+}
+function Avavaheleht10(){
+    window.location=("./Vahelehed/TTHK.html")
+}
+function Avavaheleht3(){
+    window.location=("./Vahelehed/SKA.html")
+}
+function Avavaheleht7(){
+    window.location=("./Vahelehed/EMU.html")
+}
+function Avavaheleht8(){
+    window.location=("./Vahelehed/EMTA.html")
+}
+function Avavaheleht2(){
+    window.location=("./Vahelehed/TalTech.html")
+}
+function Avavaheleht4(){
+    window.location=("./Vahelehed/LENakadem.html")
+}
+function Avavaheleht5(){
+    window.location=("./Vahelehed/KunstiAK.html")
+}
+function Avavaheleht6(){
+    window.location=("./Vahelehed/tktk.html")
+}
+function Avavaheleht11(){
+    window.location=("./Vahelehed/KaitseAK.html")
+}
+function Avavaheleht12(){
+    window.location=("./Vahelehed/TalÜl.html")
+}
